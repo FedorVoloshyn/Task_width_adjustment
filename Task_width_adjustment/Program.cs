bf093a0ba23a1eca1cba91d6f981bbafebbf7963
FedorVoloshyn <tikey.gm@gmail.com>
2015-09-23 19:40:33 +0300

    Выводим список слов

--- Task_width_adjustment/Program.cs
+++ Task_width_adjustment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task_width_adjustment
 {
@@ -10,6 +11,28 @@ namespace Task_width_adjustment
     {
         static void Main(string[] args)
         {
+            if (File.Exists(@"C:\Users\ФЁДОР\Documents\Visual Studio 2013\Projects\Task_width_adjustment\Task_width_adjustment\in.txt"))
+            {
+                string[] lines = File.ReadAllLines(@"C:\Users\ФЁДОР\Documents\Visual Studio 2013\Projects\Task_width_adjustment\Task_width_adjustment\in.txt", Encoding.Default);
+                if (lines.Length == 2)
+                {
+                    // Show text from file 'in.txt'
+                    Console.WriteLine("File 'in.txt' contains: ");
+                    foreach(string x in lines)
+                        Console.WriteLine(x);
+                    Console.WriteLine("\n");
+                    
+                    int width = Convert.ToInt32(lines[0]);
+                    string text = lines[1];
+                    char[] separators = {',', ' '};
+                    string[] words = text.Split(separators);
+
+                    foreach (string x in words)
+                        Console.WriteLine(x);
+                }
+                else
+                    Console.WriteLine("Incorrect number of lines in 'in.txt'!");
+            }
         }
     }
 }
