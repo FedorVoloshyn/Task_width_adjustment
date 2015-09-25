63fffefa8ac8c8b9c2542e553e3cbf1dca4eaafc
FedorVoloshyn <tikey.gm@gmail.com>
2015-09-25 11:01:49 +0300

    Тесты.
    Различные входные данные, ограничение минимальной ширины (width > 3).

--- Task_width_adjustment/Program.cs
+++ Task_width_adjustment/Program.cs
@@ -35,73 +35,76 @@ namespace Task_width_adjustment
                     List<string> finalStrings = new List<string>();
                     bool isEnd = false;
                     int countOfLettersInString = 0, i = 0, j, avaluableSpaces = 0, startWord = 0, countOfWordsInLine = 0;
-
-                    while (!isEnd)
+                    if (width >= 3)
                     {
-                        while (countOfLettersInString <= width && countOfLettersInString + words[i].Length <= width)
+                        while (!isEnd)
                         {
-                            countOfLettersInString += words[i].Length + 1;
-                            i++;
-                            countOfWordsInLine++;
+                            while (countOfLettersInString <= width && countOfLettersInString + words[i].Length <= width)
+                            {
+                                countOfLettersInString += words[i].Length + 1;
+                                i++;
+                                countOfWordsInLine++;
+                                if (i == words.Length)
+                                    break;
+                            }
+                            // Calculate spaces to enter after words
+                            avaluableSpaces = width - countOfLettersInString + countOfWordsInLine;
+
+                            // Adding spaces to words
+                            while (avaluableSpaces != 0)
+                            {
+                                if (countOfWordsInLine == 1)
+                                {
+                                    j = startWord;
+                                    while (avaluableSpaces != 0)
+                                    {
+                                        if (avaluableSpaces > 0)
+                                        {
+                                            words[j] += ' ';
+                                            avaluableSpaces--;
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    for (j = startWord; j < i - 1; j++)
+                                    {
+                                        if (avaluableSpaces > 0)
+                                        {
+                                            words[j] += ' ';
+                                            avaluableSpaces--;
+                                        }
+                                    }
+                                }
+                            }
+
+                            // Compiling strings
+                            string tmp = "";
+                            for (j = startWord; j < i; j++)
+                            {
+                                tmp += words[j];
+                            }
+
+                            finalStrings.Add(tmp);
+
+                            // Reset variables
+                            startWord = i;
+                            countOfLettersInString = 0;
+                            avaluableSpaces = 0;
+                            countOfWordsInLine = 0;
+
+                            // Check is finish
                             if (i == words.Length)
-                                break;
-                        }
-                        // Calculate spaces to enter after words
-                        avaluableSpaces = width - countOfLettersInString + countOfWordsInLine;
-
-                        // Adding spaces to words
-                        while (avaluableSpaces != 0)
-                        {
-                            if (countOfWordsInLine == 1)
-                            {
-                                j = startWord;
-                                while (avaluableSpaces != 0)
-                                {
-                                    if (avaluableSpaces > 0)
-                                    {
-                                        words[j] += ' ';
-                                        avaluableSpaces--;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                for (j = startWord; j < i - 1; j++)
-                                {
-                                    if (avaluableSpaces > 0)
-                                    {
-                                        words[j] += ' ';
-                                        avaluableSpaces--;
-                                    }
-                                }
-                            }
+                                isEnd = true;
                         }
 
-                        // Compiling strings
-                        string tmp = "";
-                        for (j = startWord; j < i; j++)
-                        {
-                            tmp += words[j];
-                        }
-
-                        finalStrings.Add(tmp);
-
-                        // Reset variables
-                        startWord = i;
-                        countOfLettersInString = 0;
-                        avaluableSpaces = 0;
-                        countOfWordsInLine = 0;
-
-                        // Check is finish
-                        if (i == words.Length)
-                            isEnd = true;
+                        Console.WriteLine("\n");
+                        foreach (string x in finalStrings)
+                            Console.WriteLine(x);
+                        File.WriteAllLines(@"C:\Users\ФЁДОР\Documents\Visual Studio 2013\Projects\Task_width_adjustment\Task_width_adjustment\out.txt", finalStrings);
                     }
-
-                    Console.WriteLine("\n");
-                    foreach (string x in finalStrings)
-                        Console.WriteLine(x);
-
-                    File.WriteAllLines(@"C:\Users\ФЁДОР\Documents\Visual Studio 2013\Projects\Task_width_adjustment\Task_width_adjustment\out.txt", finalStrings);
+                    else
+                        Console.WriteLine("Incorrect width.");
                 }
                 else
                     Console.WriteLine("Incorrect number of lines in 'in.txt'!");
